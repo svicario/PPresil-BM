49da35573b1e99b1ef116b3f47314034cb2bd1d3
svicario <saverio.vicario@gmail.com>
2020-09-30 12:03:04 +0200

    change to individual in vlab

--- VlabRunner.py
+++ VlabRunner.py
@@ -2,10 +2,12 @@ import subprocess
 import os
 import json
 
-print(os.listdir("./DataInput")")
-print(os.listdir("./")")
-subprocess.run(["unzip","./DataInput/"]+os.listdir("./DataInput")[0])
 
+#print(os.listdir("./DataInput"))
+print(os.listdir("./"))
+#subprocess.run(["unzip","./DataInput/"]+os.listdir("./DataInput")[0])
+subprocess.run(["unzip","DataInput.zip", "-d","./DataInput"])
+print(os.listdir("./"))
 try:
     inputfile=[ x for x in os.listdir("./DataInput") if x.split(".")[-1].lower() in ["tif", "tiff", "nc","envi"]][0]
 except IndexError:
