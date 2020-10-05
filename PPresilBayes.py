7a031f3df07f9dd36de1193a84c153b1558a0220
svicario <saverio.vicario@gmail.com>
2020-10-05 14:30:21 +0200

    add copyrigth

--- PPresilBayes.py
+++ PPresilBayes.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3.6
+
+# Copyrigth 2019 Saverio Vicario 
+# This file is part of PPresil-BM.
+
+    # PPresil-BM is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # PPresil-BM is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with PPresil-BM.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 from sklearn.preprocessing import FunctionTransformer, PolynomialFeatures
 from linear_model import BayesianLinearModel, _model_evidence, _negative_log_marginal_likelihood
 from patsy.contrasts import Sum, Treatment
